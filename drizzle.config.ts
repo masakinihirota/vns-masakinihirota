2fb649026d5f23e00b9e3c20b411d9de083b254e
masakinihirota <masakinihirota@gmail.com>
2025-03-19 20:00:34 +0900

    Supabaseの設定を追加し、環境変数の例を更新

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,30 @@
-import type { Config } from 'drizzle-kit';
+// import type { Config } from 'drizzle-kit';
 
-export default {
-  schema: './lib/db/schema.ts',
-  out: './lib/db/migrations',
-  dialect: 'postgresql',
-  dbCredentials: {
-    url: process.env.POSTGRES_URL!,
-  },
-} satisfies Config;
+// export default {
+//   schema: './lib/db/schema.ts',
+//   out: './lib/db/migrations',
+//   dialect: 'postgresql',
+//   dbCredentials: {
+//     url: process.env.POSTGRES_URL!,
+//   },
+// } satisfies Config;
+
+// 👆 元のコード
+// 👇 Supabase(ローカル)の設定に変更
+
+import { config } from "dotenv";
+import { defineConfig } from "drizzle-kit";
+config({ path: ".env" });
+
+export default defineConfig({
+	// フォルダ内にあるスキーマファイルを読み込む
+	schema: "./lib/db/schema",
+	// Supabase へのマイグレーションファイルを出力するディレクトリ
+	out: "./supabase/migrations",
+	dialect: "postgresql",
+	dbCredentials: {
+		url: process.env.POSTGRES_URL!,
+	},
+});
+
+
