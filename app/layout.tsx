b974972d5784c3f9292abd6fbb99e7ce345e39e8
masakinihirota <masakinihirota@gmail.com>
2025-03-25 11:28:16 +0900

    ダークモードの土台をインストール

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import { getUser } from '@/lib/db/queries';
 import type { Metadata, Viewport } from 'next';
 import { Manrope } from 'next/font/google';
 
+import { ThemeProvider } from "@/components/theme-provider";
+
 export const metadata: Metadata = {
   title: 'Next.js SaaS Starter',
   description: 'Get started quickly with Next.js, Postgres, and Stripe.',
@@ -20,15 +22,23 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  let userPromise = getUser();
+  const userPromise = getUser();
 
   return (
 			<html
-				lang="en"
+				lang="ja"
 				className={`bg-white dark:bg-gray-950 text-black dark:text-white ${manrope.className}`}
+				suppressHydrationWarning
 			>
 				<body className="min-h-[100dvh] bg-gray-50">
-					<UserProvider userPromise={userPromise}>{children}</UserProvider>
+					<ThemeProvider
+						attribute="class"
+						defaultTheme="system"
+						enableSystem
+						disableTransitionOnChange
+					>
+						<UserProvider userPromise={userPromise}>{children}</UserProvider>
+					</ThemeProvider>
 				</body>
 			</html>
 		);
