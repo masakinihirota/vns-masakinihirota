7a873458ec2640403230ffdae7e7378742e473ca
masakinihirota <masakinihirota@gmail.com>
2025-04-30 19:37:11 +0900

    add Shadcn UI Blocks

---
+++ src/app/(unauth)/team-03/page.tsx
@@ -0,0 +1,11 @@
+import Team03Page from "@/components/shadcnui-blocks/team-03/team-03";
+
+export const Page = () => {
+  return (
+    <div>
+      <Team03Page />
+    </div>
+  );
+};
+
+export default Page;
