67fecbac08b6e78b5bc13e7c49e848a6d6e0543f
masakinihirota <masakinihirota@gmail.com>
2025-04-30 22:39:28 +0900

    ナビゲーションメニューとロゴコンポーネントを追加し、ナビゲーションシートを実装しました。ナビゲーションメニューにはホーム、ブログ、アバウト、お問い合わせのリンクが含まれています。モバイル表示用のメニューも追加しました。

--- src/app/(unauth)/page.tsx
+++ src/app/(unauth)/page.tsx
@@ -1,19 +1,59 @@
-import Navbar03Page from "@/components/shadcnui-blocks/navbar-03/navbar-03";
-import Hero01 from "@/components/shadcnui-blocks/hero-01";
-import Footer05Page from "@/components/shadcnui-blocks/footer-05";
-import Features01Page from "@/components/shadcnui-blocks/features-01";
-import Timeline from "@/components/shadcnui-blocks/timeline-03";
-import Testimonial05 from "@/components/shadcnui-blocks/testimonial-05";
-import FAQ03 from "@/components/shadcnui-blocks/faq-03";
-import Logos02Page from "@/components/shadcnui-blocks/logos-02/logos-02";
-import Contact01Page from "@/components/shadcnui-blocks/contact-01/contact-01";
-import Stats01Page from "@/components/shadcnui-blocks/stats-01/stats-01";
+// import Hero01 from "@/components/shadcnui-blocks/hero-01";
+// import Footer05Page from "@/components/shadcnui-blocks/footer-05";
+// import Features01Page from "@/components/shadcnui-blocks/features-01";
+// import Timeline from "@/components/shadcnui-blocks/timeline-03";
+// import Testimonial05 from "@/components/shadcnui-blocks/testimonial-05";
+// import FAQ03 from "@/components/shadcnui-blocks/faq-03";
+// import Logos02Page from "@/components/shadcnui-blocks/logos-02/logos-02";
+// import Contact01Page from "@/components/shadcnui-blocks/contact-01/contact-01";
+// import Stats01Page from "@/components/shadcnui-blocks/stats-01/stats-01";
+// import { Navbar } from "@/components/shadcnui-blocks/navbar-01";
+import { useTranslations } from "next-intl";
+import { ModeToggle } from "@/app/ModeTogglePage/mode-toggle";
+import LocaleSwitcher from "@/components/i18n/LocaleSwitcher";
+import Link from "next/link";
 
 export default function Home() {
+  // 翻訳
+  const t = useTranslations("HomePage");
+  const t2 = useTranslations("AppLayout");
+
   return (
     <>
-      <Navbar03Page />
-      <Hero01 />
+      {/* <Navbar /> */}
+      <main className="">
+        {/* 認証ページへ */}
+        <div className="w-full max-w-sm">
+          <h1>TOPページ</h1>
+          {/* 言語スイッチ */}
+          <div className="mb-4">
+            <LocaleSwitcher />
+          </div>
+          {/* 挨拶文(言語変更の確認) */}
+          <h2>{t("title")}</h2>
+          <h2>{t2("home")}</h2>
+          {/* ダークモードボタン */}
+          <Link href="./ModeTogglePage">ModeTogglePage</Link>
+          <div />
+          {/* ToggleButton */}
+          <ModeToggle />
+          <p>認証 ログインページへ</p>
+          <Link href="/login">ログインページへ</Link>
+          <div />
+          {/* ログアウト */}
+          <Link href="/logout">ログアウトページへ</Link>
+          <div />
+          {/* 言語ページ */}
+          <Link href="/lang">言語ページ</Link>
+        </div>
+        {/* Hono */}
+        <Link href="/hono">Honoページへ </Link>
+        <br />
+        {/* Team */}
+        <Link href="/team-03">Teamページへ</Link>
+      </main>
+      {/* <Navbar03Page /> */}
+      {/* <Hero01 />
       <Features01Page />
       <Timeline />
       <Testimonial05 />
@@ -21,7 +61,7 @@ export default function Home() {
       <Logos02Page />
       <Contact01Page />
       <Stats01Page />
-      <Footer05Page />
+      <Footer05Page /> */}
     </>
   );
 }
