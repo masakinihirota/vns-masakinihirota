0492287eb8ccdc13a8c71ad05da3bd230b9e0516
masakinihirota <masakinihirota@gmail.com>
2025-05-06 14:59:14 +0900

    feat: root_accountテーブルと関連するトリガーを追加し、ユーザー作成時に情報を挿入する機能を実装

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,14 @@
 import { config } from "dotenv";
-import { defineConfig } from 'drizzle-kit';
+import { defineConfig } from "drizzle-kit";
 config({ path: ".env" });
 
 export default defineConfig({
-    // フォルダ内にあるスキーマファイルを読み込む
-    schema: './schema',
-    // Supabase へのマイグレーションファイルを出力するディレクトリ
-    out: "./supabase/migrations",
-    dialect: "postgresql",
-    dbCredentials: {
-      url: process.env.POSTGRES_URL!,
-    },
+  // フォルダ内にあるスキーマファイルを読み込む
+  schema: "./drizzle_schemas",
+  // Supabase へのマイグレーションファイルを出力するディレクトリ
+  out: "./supabase/migrations",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.POSTGRES_URL!,
+  },
 });
