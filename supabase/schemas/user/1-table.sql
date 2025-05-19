a8aa762859d91a9d8447b838da790bd076f076fa
masakinihirota <masakinihirota@gmail.com>
2025-05-19 22:05:29 +0900

    DrizzleをやめてSupabaseの宣言型データベーススキーマを採用

--- supabase/schemas/user/1-table.sql
+++
@@ -1,7 +0,0 @@
-create table "user" (
-  "id" uuid primary key not null,
-  "created_at" timestamptz default now() not null,
-  "updated_at" timestamptz,
-  "email" varchar(255) unique not null,
-  "password" varchar(255) not null
-);
--- supabase/seed.sql
+++
@@ -1,6 +0,0 @@
--- トリガーの作成
-CREATE TRIGGER on_auth_user_created
-AFTER INSERT ON auth.users
-FOR EACH ROW
-EXECUTE FUNCTION public.handle_new_user();
-
