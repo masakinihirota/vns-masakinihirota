85aa1b7ed65dd9f49c7c84659eb32f114cebe4d7
masakinihirota <masakinihirota@gmail.com>
2025-08-29 02:19:29 +0900

    コピー用のテンプレートの用意

---
+++ __Template/app/template/page.tsx
@@ -0,0 +1,15 @@
+import * as template from "@/components/template"
+
+/**
+ * TemplatePage - ルーティングとコンポーネント分離の最小サンプル
+ * components/template/index.ts から一括インポート
+ */
+export default function TemplatePage() {
+	return (
+		<main className='max-w-xl mx-auto p-6'>
+			<h1 className='text-2xl font-bold mb-6'>Template Sample Page</h1>
+			<template.ComponentA />
+			<template.ComponentB />
+		</main>
+	)
+}
---
+++ __Template/components/template/index.ts
@@ -0,0 +1,5 @@
+/**
+ * Templateページで使用されるコンポーネントをまとめてエクスポートします。
+ */
+export { default as ComponentA } from "./ComponentA/component-a"
+export { default as ComponentB } from "./ComponentB/component-b"
