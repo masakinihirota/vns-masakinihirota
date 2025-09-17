bd2b4e68d9c9ce393cba115765e6bb2a312bd238
masakinihirota <masakinihirota@gmail.com>
2025-09-17 19:12:12 +0900

    Refactor: Remove unused components and links from v0.60 series; clean up codebase

--- src/components/v0/v0_60/04_user-profile.tsx
+++
@@ -1,9 +0,0 @@
-// The component has been moved and renamed from `src/components/v0/60/user-profile.tsx`.
-
-import React from 'react';
-
-const UserProfile = () => {
-  return <div>User Profile Component</div>;
-};
-
-export default UserProfile;
