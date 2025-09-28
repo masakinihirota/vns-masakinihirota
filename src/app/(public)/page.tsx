21c2f131268da4674f10524b53af938c89421e18
masakinihirota <masakinihirota@gmail.com>
2025-09-29 02:15:23 +0900

    feat: Add landing pages for VNS MVP with feature highlights, onboarding steps, and user journey details

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,89 +1,12 @@
 import Link from "next/link"
-import { useTranslations } from "next-intl"
 
-import V0_60 from "../(___sample)/v0_60"
-
-import GeminiComponents from "@/app/(___sample)/(gemini)/gemini"
-// V0_folder
-import V0_folder from "@/app/(___sample)/v0_folder"
-import { ModeToggle } from "@/app/(unauth)/ModeTogglePage/mode-toggle"
-import LocaleSwitcher from "@/components/i18n/LocaleSwitcher"
-
-// v0_folder.tsx
-
-export default function Home() {
-	// 翻訳
-	const t = useTranslations("HomePage")
-	const t2 = useTranslations("AppLayout")
-
+export default function Page() {
 	return (
-		<>
-			{/* Sample へのリンク */}
-			<Link href='/sample'>Sampleページへ</Link>
-			<br></br>
-			{/* ランディングページへのリンク */}
-			<Link href='/landing'>ランディングページ</Link>
-			<br></br>
-			GeminiComponents
-			<br></br>
-			<GeminiComponents />
-			<br></br>
-			{/* v0_60のコンポーネントを使うページへのリンク */}
-			V0_60
-			<br></br>
-			<V0_60 />
-			{/* V0_folderのコンポーネントを使うページへのリンク */}
-			V0_folder
-			<br></br>
-			<V0_folder />
-			<br></br>
-			{/* メインページへのリンク */}
-			<Link href='/main-pages'>メインページへのリンク</Link>
+		<main className=''>
+			■HOMEのページ
 			<br />
-			{/* 価格のページ */}
-			<Link href='/pricing'>価格のページ</Link>
-			{/* 認証のページ */}
-			<main className=''>
-				{/* 認証ページへ */}
-				<div className='w-full max-w-sm'>
-					<h1>TOPページ</h1>
-					{/* 言語スイッチ */}
-					<div className='mb-4'>
-						<LocaleSwitcher />
-					</div>
-					{/* 挨拶文(言語変更の確認) */}
-					<h2>{t("title")}</h2>
-					<h2>{t2("home")}</h2>
-					{/* ダークモードボタン */}
-					<Link href='./ModeTogglePage'>ModeTogglePage</Link>
-					<div />
-					{/* ToggleButton */}
-					<ModeToggle />
-					<p>認証 ログインページへ</p>
-					<Link href='/login'>ログインページへ</Link>
-					<div />
-					{/* ログアウト */}
-					<Link href='/logout'>ログアウトページへ</Link>
-					<div />
-					{/* 言語ページ */}
-					<Link href='/lang'>言語ページ</Link>
-					<div />
-					{/* プロテクトページへ */}
-					<Link href='/protected'>プロテクトページへ</Link>
-				</div>
-				{/* Hono */}
-				<Link href='/hono'>Honoページへ </Link>
-				<br />
-				{/* Team */}
-				<Link href='/team-03'>Teamページへ</Link>
-				<br />
-				{/* Root Accounts */}
-				<Link href='/root_accounts'>Root Accountsページへ</Link>
-				<br />
-				{/* template:コンポーネント */}
-				<Link href='/template'>コンポーネントのTemplateページへ</Link>
-			</main>
-			<footer className=''>footer</footer>
-		</>
+			{/* サンプルへのリンク */}
+			<Link href='/sample'>Sampleページへ</Link>
+		</main>
 	)
 }
