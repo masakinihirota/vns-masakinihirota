e97b531bbbe0bfaef82c36346b1af9d1b61c1cf1
masakinihirota <masakinihirota@gmail.com>
2025-12-11 05:00:12 +0900

    add Eslint Prettier vitest

--- vitest.config.js
+++ vitest.config.js
@@ -1,23 +1,18 @@
 import { defineConfig } from "vitest/config";
 import path from "path";
 
-
 export default defineConfig({
-    test: {
-        // include: ['src/**/*.test.tsx'],
-        include: ['src/**/*.test.{ts,tsx}'],
-        exclude: [
-            '_sample',
-            '_Template',
-        ],
-        globals: true,
-        environment: 'jsdom',
-        setupFiles: ['./tests/setup.ts']
+  test: {
+    // include: ['src/**/*.test.tsx'],
+    include: ["src/**/*.test.{ts,tsx}"],
+    exclude: ["_sample", "_Template"],
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./tests/setup.ts"],
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src')
-        }
-    }
+  },
 });
-
