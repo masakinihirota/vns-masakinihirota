b49f75705886b35772e1b2f289a3cc4e3d62cf75
masakinihirota <masakinihirota@gmail.com>
2026-03-03 23:56:49 +0900

    コンセプトページ

---
+++ src/components/concept-explanation/concept-explanation/concept-explanation.container.tsx
@@ -0,0 +1,10 @@
+import { ConceptExplanation } from "./concept-explanation";
+
+/**
+ *
+ * コンテナコンポーネント
+ * 現在は静的表示のみだが、将来的なデータ統合や状態管理の場所として用意
+ */
+export function ConceptExplanationContainer() {
+  return <ConceptExplanation />;
+}
